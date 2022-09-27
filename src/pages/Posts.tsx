4693a41645b37d67cde18b69b0f61752833aad16
ArbenitBytyqi <arbenitbytyqi7@gmail.com>
2022-09-27 16:56:56 +0200

    posts mapped

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import "./Posts.css";
+
+type Post = {
+  id: number;
+  title: string;
+  image: string;
+  content: string;
+  price: number;
+  saved: boolean;
+  toSell: boolean;
+  toBuy: boolean;
+  userId: number;
+  tags: Tag[];
+};
+
+type Tag = {
+  id: number;
+  name: string;
+  posts: Post[];
+};
 
 export default function Posts() {
-  return (
-    <div>Posts</div>
-  )
-}
+  const [posts, setPosts] = useState<Post[]>([]);
 
+  useEffect(() => {
+    fetch("http://localhost:4000/posts")
+      .then((resp) => resp.json())
+      .then((postsFromServer) => setPosts(postsFromServer));
+  }, []);
+
+  return (
+    <div className="main-div">
+      <div className="posts-div">
+        {posts.map((post) => (
+          <>
+            <h3>{post.title}</h3>
+            <img src={post.image} alt={post.title} className="image" />
+            <h3>{post.content}</h3>
+            <h3>{post.price}</h3>
+            <h3>{post.saved}</h3>
+            <h3>{post.toSell}</h3>
+            <h3>{post.toBuy}</h3>
+            <h3>{post.userId}</h3>
+            <p>
+              {post.tags.map((tag) => (
+                <>
+                  <h3>Category: {tag.name}</h3>
+                  <p>---------------------------------------------</p>
+                </>
+              ))}
+            </p>
+          </>
+        ))}
+      </div>
+    </div>
+  );
+}
